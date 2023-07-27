39d209bea5ea1444952ca936b9d6784031386fd1
Christopher020 <uchennachris020@gmail.com>
2023-07-27 11:30:07 +0100

    update2

--- stududents/models.py
+++ stududents/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     student_number = models.PositiveIntegerField()
-    firist_name = models.CharField(max_length= 50)
+    first_name = models.CharField(max_length= 50)
     last_name = models.CharField(max_length= 50)
     email = models.EmailField(max_length= 100)
     field_of_study = models.CharField(max_length= 50)
-    gta = models.FloatField()
+    gpa = models.FloatField()
 
     def __str__(self):
-        return f'student: {self.firist_name} {self.last_name}'
+        return f'student: {self.first_name} {self.last_name}'
